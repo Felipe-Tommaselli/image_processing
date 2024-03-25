2c6d47ef4dc8b799d1d8d2996f32f24a9fcec422
Felipe-Tommaselli <f.tommaselli@usp.br>
2024-03-24 22:39:11 -0300

    done

--- 1-superresolution/solution11800910.py
+++ 1-superresolution/solution11800910.py
@@ -1,162 +1,176 @@
 '''
-Name: Gianluca Capezzuto Sardinha
-NUSP: 11876933
-Course Code: SCC0651
-Year/Semester: 2024/1
-Title: Enhancement and Superresolution
+    @Name: Felipe Andrade Garcia Tommaselli
+    @Number: 11800910
+    @Course: SCC0722
 '''
 
-import numpy as np
-import imageio.v3 as iio
+# Importação das bibliotecas necessárias
+import numpy as nmp
+import imageio.v3 as iiv3
 
-def rmse(enh_image, ref_image):
-    return round(np.sqrt(np.mean((enh_image.astype(np.int16) - ref_image.astype(np.int16)) ** 2)), 4)
 
-def load_images(name):
-    images = []
+# Definição da função para imprimir arte ASCII
+def ascii_art():
+    print('/\\'*78)
+    print('    __________                                                                       __              .___       .__                                 ')           
+    print('\______   _______  ____   ____  ____   ______ __________    _____   ____   _____/  |_ ____     __| _/____   |__| _____ _____    ____   ____   ____   ______')
+    print('|     ___\_  __ \/  _ \_/ ____/ __ \ /  ___//  ___\__  \  /     \_/ __ \ /    \   __/  _ \   / __ _/ __ \  |  |/     \\__  \  / ___\_/ __ \ /    \ /  ___/')
+    print('|    |    |  | \(  <_> \  \__\  ___/ \___ \ \___ \ / __ \|  Y Y  \  ___/|   |  |  |(  <_> ) / /_/ \  ___/  |  |  Y Y  \/ __ \/ /_/  \  ___/|   |  \\___ \ ')
+    print('|____|    |__|   \____/ \___  \___  /____  /____  (____  |__|_|  /\___  |___|  |__| \____/  \____ |\___  > |__|__|_|  (____  \___  / \___  |___|  /____  >')
+    print('                            \/    \/     \/     \/     \/      \/     \/     \/                  \/    \/           \/     \/_____/      \/     \/    ')
+    print('/\\'*78)
 
-    for type in range(0, 4):
-        image = iio.imread(str(name) + str(type) + '.png')
-        images.append(image)
+# Definição da função para calcular o histograma de uma imagem
+def histo(image, num_levels = 256):
+    hist = nmp.zeros(num_levels).astype(int)
 
-    return images
-
-def map_indices(i, j):
-    if i % 2 == 0 and j % 2 == 0:
-        return 0
-    elif i % 2 == 1 and j % 2 == 0:
-        return 1
-    elif i % 2 == 0 and j % 2 == 1:
-        return 2
-    elif i % 2 == 1 and j % 2 == 1:
-        return 3
-
-def superresolution(low_images, high_image):
-    h, w = high_image.shape
-    low_image_upscaled = np.zeros((h, w)) 
-
-    for i in range(0, h):
-        for j in range(0, w):
-            low_image_upscaled[i, j] = low_images[map_indices(i, j)][i // 2, j // 2]
-
-    low_image_upscaled = low_image_upscaled.astype(np.uint8)
-
-    return rmse(low_image_upscaled, high_image)
-
-def histogram(image, no_levels = 256):
-    hist = np.zeros(no_levels).astype(int)
-
-    for i in range(no_levels):
-        no_pixel_value_i = np.sum(image == i)
+    for i in range(num_levels):
+        num_pix_val_i = nmp.sum(image == i)
     
-        hist[i] = no_pixel_value_i
+        hist[i] = num_pix_val_i
 
     return hist
 
-def single_cumulative_histogram(images, no_levels = 256):
-    hist_transf_list = []
+# Definição da função para realizar a equalização de histograma em uma única imagem
+def sole_cumulative_histo(images, num_levels = 256):
+    histo_transf_list = []
     image_eq_list = []
 
     for image in images:
-        hist = histogram(image, no_levels)
+        hist = histo(image, num_levels)
 
-        histC = np.zeros(no_levels).astype(int)
+        histC = nmp.zeros(num_levels).astype(int)
         histC[0] = hist[0]
 
-        for i in range(1, no_levels):
+        for i in range(1, num_levels):
             histC[i] = hist[i] + histC[i - 1]
 
-        hist_transf = np.zeros(no_levels).astype(np.uint8)
+        hist_transf = nmp.zeros(num_levels).astype(nmp.uint8)
 
         N, M = image.shape
-        image_eq = np.zeros([N, M]).astype(np.uint8)
+        image_eq = nmp.zeros([N, M]).astype(nmp.uint8)
 
-        for z in range(no_levels):
-            s = ((no_levels - 1) / float(M * N)) * histC[z]
+        for z in range(num_levels):
+            s = ((num_levels - 1) / float(M * N)) * histC[z]
             hist_transf[z] = s
 
-            image_eq[np.where(image == z)] = s
+            image_eq[nmp.where(image == z)] = s
 
-        hist_transf_list.append(hist_transf)
+        histo_transf_list.append(hist_transf)
         image_eq_list.append(image_eq)
 
-    return image_eq_list, hist_transf_list
+    return image_eq_list, histo_transf_list
 
-def joint_cumulative_histogram(images, no_levels = 256):
-    hist_images = []    
-    hist_transf_list = []
+# Definição da função para realizar a super-resolução
+def hyper_resolution(low_images, high_image):
+    h, w = high_image.shape
+    low_image_up = nmp.zeros((h, w)) 
+
+    for _ in range(0, h):
+        for __ in range(0, w):
+            if _ % 2 == 0 and __ % 2 == 0:
+                index = 0
+            elif _ % 2 == 1 and __ % 2 == 0:
+                index = 1
+            elif _ % 2 == 0 and __ % 2 == 1:
+                index = 2
+            elif _ % 2 == 1 and __ % 2 == 1:
+                index = 3
+            low_image_up[_, __] = low_images[index][_ // 2, __ // 2]
+        low_image_up = low_image_up.astype(nmp.uint8)
+
+    output = round(nmp.sqrt(nmp.mean((low_image_up.astype(nmp.int16) - high_image.astype(nmp.int16)) ** 2)), 4)
+
+    return output
+
+# Definição da função para realizar a equalização de histograma conjunta
+def joint_cumulative_histo(images, num_levels = 256):
+    histo_images = []    
+    histo_transf_list = []
     image_eq_list = []
 
     for image in images:
-        hist = histogram(image, no_levels)
+        hist = histo(image, num_levels)
 
-        histC = np.zeros(no_levels).astype(int)
+        histC = nmp.zeros(num_levels).astype(int)
         histC[0] = hist[0]
 
-        for i in range(1, no_levels):
+        for i in range(1, num_levels):
             histC[i] = hist[i] + histC[i - 1]
 
-        hist_images.append(histC)
+        histo_images.append(histC)
 
-    histC = np.zeros(no_levels).astype(int)
+    histC = nmp.zeros(num_levels).astype(int)
 
     for i in range(0, 4):
-        histC += hist_images[i]
+        histC += histo_images[i]
 
     for image in images:
-        hist_transf = np.zeros(no_levels).astype(np.uint8)
+        hist_transf = nmp.zeros(num_levels).astype(nmp.uint8)
 
         N, M = image.shape
-        image_eq = np.zeros([N, M]).astype(np.uint8)
+        image_eq = nmp.zeros([N, M]).astype(nmp.uint8)
 
-        for z in range(no_levels):
-            s = ((1/4) * (no_levels - 1) / float(M * N)) * histC[z]
+        for z in range(num_levels):
+            s = ((1/4) * (num_levels - 1) / float(M * N)) * histC[z]
             hist_transf[z] = s
 
-            image_eq[np.where(image == z)] = s
+            image_eq[nmp.where(image == z)] = s
 
-        hist_transf_list.append(hist_transf)
+        histo_transf_list.append(hist_transf)
         image_eq_list.append(image_eq)
 
-    return image_eq_list, hist_transf_list
+    return image_eq_list, histo_transf_list
 
-def gamma_correction(images, gamma):
+# Definição da função para realizar a correção gama em imagens
+def gamma_correct(images, gamma_val):
     image_eq_list = []
 
     for image in images:
-        image_eq = (255 * ((image / 255) ** (1 / gamma))).astype(np.uint8) 
+        image_eq = (255 * ((image / 255) ** (1 / gamma_val))).astype(nmp.uint8) 
         image_eq_list.append(image_eq)
 
     return image_eq_list
 
+# Definição da função para selecionar o tipo de pré-processamento a ser aplicado nas imagens
+def select_kp(f_option, low_images, gamma_value):
+    if f_option == 0:
+        out_image = low_images
+
+    elif f_option == 1:
+        out_image, _ = sole_cumulative_histo(low_images)
+
+    elif f_option == 2:
+        out_image, _ = joint_cumulative_histo(low_images)
+
+    elif f_option == 3:
+        out_image = gamma_correct(low_images, gamma_value)
+    
+    return out_image
+
+
+# Função principal que executa o código
 if __name__ == "__main__":
-    imagelow = input().rstrip()
-    imagehigh = input().rstrip()
-    f = int(input().rstrip())
-    gamma = float(input().rstrip())
+    # Chamada para imprimir arte ASCII
+    ascii_art()
+    # Entrada dos nomes das imagens e outras informações pelo usuário
+    image_low_res = input().rstrip()
+    image_high_res = input().rstrip()
+    f_option = int(input().rstrip())
+    gamma_value = float(input().rstrip())
 
-    low_images = load_images(imagelow)
-    imagehigh = iio.imread(imagehigh)
+    # Carregamento das imagens de baixa resolução e da imagem de alta resolução
+    low_images = list()
+    for typ in range(0, 4):
+        image = iiv3.imread('test_cases/' + str(image_low_res) + str(typ) + '.png')
+        low_images.append(image)
+    image_high_res = iiv3.imread('test_cases/' + image_high_res)
 
-    if f == 0:
-        error = superresolution(low_images, imagehigh)
+    # Impressão do resultado final
+    print('\nErro final:', end=' ')
 
-        print(error)
-    elif f == 1:
-        enhanced_images, _ = single_cumulative_histogram(low_images)
+    # Seleção do tipo de pré-processamento e aplicação
+    out_image = select_kp(f_option, low_images, gamma_value)
+    error = hyper_resolution(out_image, image_high_res)
 
-        error = superresolution(enhanced_images, imagehigh)
-
-        print(error)
-    elif f == 2:
-        enhanced_images, _ = joint_cumulative_histogram(low_images)
-
-        error = superresolution(enhanced_images, imagehigh)
-
-        print(error)
-    elif f == 3:
-        enhanced_images = gamma_correction(low_images, gamma)
-
-        error = superresolution(enhanced_images, imagehigh)
-
-        print(error)
+    print(error)
